5ca341e7c7a3ff07d526fa74a7578ed2804a5791
Luca Mirea <lucapetre2017@gmail.com>
2025-01-12 17:16:11 +0200

    Signed?
    Signed-off-by: Luca Mirea <lucapetre2017@gmail.com>

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import os           # environment variables
 import inspect      # call stack inspection
 import random       # dumb random number generator
  
+
 from discord.ext import commands    # Bot class and utils
  
 ################################################################################
